d6e5b4411b3cfe41699e10207254877c8806ce2e
Josh <jkim5674@gmail.com>
2015-10-29 14:57:44 -0700

    initial commit

---
+++ server/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  
+  validates :name, :email, presence: true
+  validates :password, presence: true, on: create
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+end
+end
